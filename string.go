29c4eaf66907352553c91c5008fadf44fb17864b
Wellington Gadelha <contato.informeai@gmail.com>
2021-08-15 06:38:30 -0300

    add many methods of string.

--- string.go
+++ string.go
@@ -1,14 +1,49 @@
 package gohelps
 
+import (
+	"errors"
+)
+
 //String is struct base for methods the object string.
 type String struct {
 	base string
 }
 
+//var errors
+var (
+	ErrIndexOutOfRange = errors.New("index out of range.")
+	ErrEmptyValue      = errors.New("this empty value.")
+)
+
 //CharAt return of rune by index.
-func (s String) CharAt(i int) string {
+func (s String) CharAt(i int) (string, error) {
 	if i > len(s.base)-1 || i < 0 {
-		return ""
+		return "", ErrIndexOutOfRange
 	}
-	return string(s.base[i])
+	return string(s.base[i]), nil
+}
+
+//CharCodeAt return of byte by index.
+func (s String) CharCodeAt(i int) (byte, error) {
+	if i > len(s.base)-1 || i < 0 {
+		return byte(0), ErrIndexOutOfRange
+	}
+	return s.base[i], nil
+}
+
+//Concat return of union the two string.
+func (s String) Concat(str string) (string, error) {
+	if len(str) == 0 {
+		return "", ErrEmptyValue
+	}
+	return s.base + str, nil
+}
+
+//EndsWith return the bool if ends word match of string.
+func (s String) EndsWith(str string) (bool, error) {
+	if len(str) == 0 {
+		return false, ErrEmptyValue
+	}
+	lastIndex := len(s.base)
+	return s.base[lastIndex-len(str):] == str, nil
 }
--- string_test.go
+++ string_test.go
@@ -9,9 +9,42 @@ import (
 func TestCharAt(t *testing.T) {
 	str := "informeai"
 	s := NewString(str)
-	ru := s.CharAt(3)
-	if ru == "" {
-		t.Errorf("TestCharAt(): got -> %v, want: any string", ru)
+	ru, err := s.CharAt(3)
+	if err != nil {
+		t.Errorf("TestCharAt(): got -> %v, want: nil", err)
 	}
 	log.Println(ru)
+}
+
+//go test -v -run ^TestCharCodeAt
+func TestCharCodeAt(t *testing.T) {
+	str := "informeai"
+	s := NewString(str)
+	bs, err := s.CharCodeAt(3)
+	if err != nil {
+		t.Errorf("TestCharCodeAt(): got -> %v, want: nil", err)
+	}
+	log.Println(bs)
+}
+
+//go test -v -run ^TestConcat
+func TestConcat(t *testing.T) {
+	str := "hello"
+	s := NewString(str)
+	cs, err := s.Concat(", word!")
+	if err != nil {
+		t.Errorf("TestConcat(): got -> %v, want: nil", err)
+	}
+	log.Println(cs)
+}
+
+//go test -v -run ^TestEndsWith
+func TestEndsWith(t *testing.T) {
+	str := "I'm sorry doctor gopher."
+	s := NewString(str)
+	es, err := s.EndsWith("gopher.")
+	if err != nil {
+		t.Errorf("TestEndsWith(): got -> %v, want: nil", err)
+	}
+	log.Println(es)
 }
